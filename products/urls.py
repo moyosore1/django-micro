fa95413778a8b270f8d17c22d0d3395bb9be5439
moyosore1 <35351716+moyosore1@users.noreply.github.com>
2021-10-19 09:24:32 -0400

    added models

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import ProductViewSet, UserAPIView
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='products')
+
+urlpatterns = [
+    path('user/', UserAPIView.as_view()),
+    path('products', include(router.urls)),
+]
